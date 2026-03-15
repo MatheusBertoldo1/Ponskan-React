7634eda859d42f4f48684150e260d67a3efa9a33
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2026-03-15 19:54:34 -0300

    atualizando navbar

--- src/components/public/sections/Navbar/index.tsx
+++ src/components/public/sections/Navbar/index.tsx
@@ -5,8 +5,8 @@ import { NavbarLinks } from "./NavbarLinks"
 export const Navbar = () => {
     return(
         <nav className="fixed inset-x-0 top-0 z-10 py-2 bg-white border-b border-black/5">
-            <Container size="xl"> 
-                <div className="flex">
+            <Container> 
+                <div className="flex flex-1 px-4">
                     <ColoredLogo className="h-8 inline"/>
                     <NavbarLinks />
                 </div>
