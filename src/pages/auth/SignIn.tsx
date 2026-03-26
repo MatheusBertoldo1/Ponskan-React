658910677c6805e1c72d52299ee567707b59cff9
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2026-03-25 22:13:22 -0300

    autalizando pg signup

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -3,41 +3,44 @@ import { Link } from "react-router-dom"
 import { FormInput } from "../../components/inputs/FormInput"
 
 export const SignIn = () => (
-        <div className="flex  w-full lg:max-w-[60vw] h-full max-h-120 overflow-hidden bg-white border border-black/10 rounded-3xl">
-            <div className="flex flex-col flex-1 relative p-8 border-r border-black/10">
-                
-                <div className="flex absolute w-full h-25 pl-8 items-center left-0 top-0 bg-amber-400">
-                    <Link to={"/"}>
-                        <LogoCurrentColor className="w-10 h-fit text-white"/>
-                    </Link>
+        <div className="flex flex-col w-full lg:max-w-[60vw] h-full max-h-120 overflow-hidden bg-white border border-black/10 rounded-3xl">
+            <div className="flex  w-full h-25 pl-8 items-center left-0 top-0 bg-amber-400">
+                <Link to={"/"}>
+                    <LogoCurrentColor className="w-10 h-fit text-white"/>
+                </Link>
+            </div>
+    
+            <div className="flex flex-1">
+                <div className="flex flex-col flex-1 relative p-8 border-r border-black/10">
+    
+    
+                    <div className="flex flex-col gap-2 pt-25 select-none">
+                        <p className="text-4xl text-gray-700 font-medium font-inter">Faça login</p>
+                        <p className="text-gray-800 font-inter">Acesse sua conta <span className="text-amber-400">Ponskan</span></p>
+                    </div>
                 </div>
-
-                <div className="flex flex-col gap-2 pt-25 select-none">
-                    <p className="text-4xl text-gray-700 font-medium font-inter">Faça login</p>
-                    <p className="text-gray-800 font-inter">Acesse sua conta <span className="text-amber-400">Ponskan</span></p>
+    
+                <div className="flex flex-1 px-8 items-center">
+                    <form action="" method="post" className="flex flex-col w-full gap-8">
+                        <p className="text-xl text-gray-700 font-inter select-none">Entre com suas credenciais</p>
+    
+                        <div className="flex flex-col gap-8">
+                            <FormInput id="userEmail" label="Email" type="email"/>
+                            <FormInput id="userPassword" label="Senha" type="password"/>
+                        </div>
+    
+    
+                        <div className="flex gap-8 justify-end">
+                            <Link to={"/auth/signup"} className="py-2 px-4 text-sm/6 text-gray-700 font-inter rounded-full cursor-pointer select-none border border-black/10 hover:bg-gray-50">Criar uma conta</Link>
+                            <button type="submit" className="py-2 px-4 text-sm/6 text-white font-inter bg-amber-400 rounded-full cursor-pointer select-none transition-colors hover:bg-amber-400/80">Acessar</button>
+                        </div>
+    
+                        <Link to={""} className="w-fit px-2 text-sm font-inter text-amber-500 cursor-pointer rounded-full transition-all select-none hover:bg-gray-50">
+                            Esqueceu o e-mail ou senha?
+                        </Link>
+                    </form>
                 </div>
             </div>
-
-            <div className="flex flex-1 px-8 items-center">
-                <form action="" method="post" className="flex flex-col w-full gap-8">
-                    <p className="text-xl text-gray-700 font-inter select-none">Entre com suas credenciais</p>
-
-                    <div className="flex flex-col gap-8">
-                        <FormInput id="userEmail" label="Email" type="email"/>
-                        <FormInput id="userPassword" label="Senha" type="password"/>
-                    </div>
-
-                    
-                    <div className="flex gap-8 justify-end">
-                        <Link to={"/auth/signup"} className="py-2 px-4 text-sm/6 text-gray-700 font-inter rounded-full cursor-pointer select-none border border-black/10 hover:bg-gray-50">Criar uma conta</Link>
-                        <button type="submit" className="py-2 px-4 text-sm/6 text-white font-inter bg-amber-400 rounded-full cursor-pointer select-none transition-colors hover:bg-amber-400/80">Acessar</button>
-                    </div>
-
-                    <Link to={""} className="w-fit px-2 text-sm font-inter text-amber-500 cursor-pointer rounded-full transition-all select-none hover:bg-gray-50">
-                        Esqueceu o e-mail ou senha?
-                    </Link>
-                </form>
-            </div>  
         </div>
     
 )
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,91 +1,152 @@
-import { Logotipo } from "../../assets/svg"
+import { Logotipo, Logo } from "../../assets/svg"
 import { Link } from "react-router-dom"
 import { FormInput } from "../../components/inputs/FormInput"
 import { FormSelect } from "../../components/inputs/FormSelect"
+import { useState } from "react"
 
 export const SignUp = () => {
+    const [stageForm, setStageForm] = useState(0)
+
     return(
-        <div className="flex flex-col w-full lg:max-w-[60vw] h-fit overflow-hidden select-none bg-white border border-black/10 rounded-3xl">
+        <div className="flex flex-col w-full lg:max-w-[70vw] min-h-[90vh] overflow-hidden select-none bg-white border border-black/10 rounded-3xl">
             <div className="flex justify-center items-center h-fit py-4 bg-amber-400 border-b border-black/10">
                 <p className="text-4xl font-inter font-medium text-white">Junte-se a nós</p>
             </div>
+            
+            {/* Contagem de etapas do form */}
+            <p className="px-4 pt-1 text-xs/6 text-end text-gray-700 font-inter">Etapa {stageForm + 1} de  5</p>
 
-            <form  method="post" action="" className="flex h-full border-b border-black/10">
-                <div className="flex flex-1 flex-col p-10 gap-8 border-r border-black/10">
-                    <p className="text-gray-700 font-inter font-medium mb-2">Dados pessoais</p>
-                    <FormInput id="userName" label="Nome Completo" required/>
-                    <FormInput id="userEmail" label="E-mail" required/>
-                      
-                    <FormSelect 
-                    id="typeUser" 
-                    name="typeUser" 
-                    className="p-4 font-inter outline-none text-gray-700 rounded-md border border-black/10 focus:border-amber-400"
-                    required
-                    labelText="Qual tipo de perfil mais se adequa a você?"
-                    labelClassName="text-gray-700 font-inter"
-                    options={[
-                        { optionValue: "produtorRural", content: "Produtor Rural" },
-                        { optionValue: "agronomo", content: "Agrônomo" },
-                        { optionValue: "consultor", content: "Consultor" },
-                        { optionValue: "agronomo", content: "Agrônomo" },
-                        { optionValue: "estudante", content: "Estudante" },
-                        { optionValue: "pequenoProdutor", content: "Pequeno produtor" },
-                        { optionValue: "other", content: "Outro" }
-                    ]}/>
-
-                    <FormSelect 
-                        id="uf" 
-                        name="uf" 
-                        className="p-4 font-inter outline-none text-gray-700 rounded-md border border-black/10 focus:border-amber-400"
-                        required
-                        labelText="Em qual estado você reside?"
-                        labelClassName="text-gray-700 font-inter"
-                        options={[
-                            { optionValue: "AC", content: "Acre" },
-                            { optionValue: "AL", content: "Alagoas" },
-                            { optionValue: "AP", content: "Amapá" },
-                            { optionValue: "AM", content: "Amazonas" },
-                            { optionValue: "BA", content: "Bahia" },
-                            { optionValue: "CE", content: "Ceará" },
-                            { optionValue: "DF", content: "Distrito Federal" },
-                            { optionValue: "ES", content: "Espírito Santo" },
-                            { optionValue: "GO", content: "Goiás" },
-                            { optionValue: "MA", content: "Maranhão" },
-                            { optionValue: "MT", content: "Mato Grosso" },
-                            { optionValue: "MS", content: "Mato Grosso do Sul" },
-                            { optionValue: "MG", content: "Minas Gerais" },
-                            { optionValue: "PA", content: "Pará" },
-                            { optionValue: "PB", content: "Paraíba" },
-                            { optionValue: "PR", content: "Paraná" },
-                            { optionValue: "PE", content: "Pernambuco" },
-                            { optionValue: "PI", content: "Piauí" },
-                            { optionValue: "RJ", content: "Rio de Janeiro" },
-                            { optionValue: "RN", content: "Rio Grande do Norte" },
-                            { optionValue: "RS", content: "Rio Grande do Sul" },
-                            { optionValue: "RO", content: "Rondônia" },
-                            { optionValue: "RR", content: "Roraima" },
-                            { optionValue: "SC", content: "Santa Catarina" },
-                            { optionValue: "SP", content: "São Paulo" },
-                            { optionValue: "SE", content: "Sergipe" },
-                            { optionValue: "TO", content: "Tocantins" }
-                        ]}
-                    />
+            <div className="flex flex-1 p-8 border-b border-black/10">
+                <div className="flex flex-1 flex-col gap-4 text-gray-700 font-inter">
+                    <Link to={"/"} className="w-fit">
+                        <Logo className="h-12 w-fit" />
+                    </Link>
+                    <h1 className=" text-4xl font-medium">Crie uma conta <br/> Ponskan</h1>
+                    <p>Preencha os campos solicitados</p>
                 </div>
 
-                <div className="flex flex-1 flex-col p-10 gap-8">
-                    <p className="text-gray-700 font-inter font-medium mb-2">Dados empresariais (opcional)</p>
-                    <FormInput id="bussinesName" label="Nome da fazenda"/>
+                <form action="/" method="get" className="flex flex-1 flex-col gap-6 text-gray-700 font-inter">
+                    {stageForm === 0 && (
+                        <>
+                        <p className=" font-medium mb-2 ">Dados pessoais</p>
+                        <FormInput id="userName" label="Nome Completo" required/>
+                        <FormInput id="userEmail" label="E-mail" required/>
+                        <input type="button" value="Avançar" onClick={()=>setStageForm(stageForm + 1)} className="self-end text-white text-sm/6 px-4 py-2 rounded-full cursor-pointer bg-amber-400 hover:bg-amber-400/80"/>
+                        </>
+                    )}
+                    {
+                        stageForm === 1 && (
+                            <>
+                            <p className=" font-medium mb-2 ">Dados pessoais</p>
+                            
+                            <FormSelect 
+                            id="typeUser" 
+                            name="typeUser" 
+                            className="p-4 font-inter outline-none text-gray-700 rounded-md border border-black/10 focus:border-amber-400"
+                            required
+                            labelText="Qual tipo de perfil mais se adequa a você?"
+                            labelClassName="text-gray-700 font-inter"
+                            options={[
+                                { optionValue: "produtorRural", content: "Produtor Rural" },
+                                { optionValue: "agronomo", content: "Agrônomo" },
+                                { optionValue: "consultor", content: "Consultor" },
+                                { optionValue: "agronomo", content: "Agrônomo" },
+                                { optionValue: "estudante", content: "Estudante" },
+                                { optionValue: "pequenoProdutor", content: "Pequeno produtor" },
+                                { optionValue: "other", content: "Outro" }
+                            ]}/>
 
-                    <p className="text-gray-700 font-inter font-medium mb-2">Segurança da conta</p>
-                    <FormInput id="password" label="Senha"/>
-                    <FormInput id="confirmPassword" label="Confirmar"/>
-                    
-                    <div className="flex flex-col gap-8 justify-end">
-                        <button type="submit" className="py-2 px-4 text-sm/6 text-white font-inter bg-amber-400 rounded-full cursor-pointer select-none transition-colors hover:bg-amber-400/80">Criar conta</button>
-                        <Link to={"/auth/signin"} className="w-fit px-1 text-sm/6 text-amber-500 font-inter rounded-full cursor-pointer select-none hover:bg-gray-100">Entrar em uma conta existente</Link>
-                    </div>
-                </div>
-            </form>
+                            <div className="flex flex-1 items-end justify-end gap-6">
+                                <input type="button" value="Retornar" onClick={()=>setStageForm(stageForm - 1)} className="text-gray-800 text-sm/6 px-4 py-2 rounded-full border border-black/10 cursor-pointer bg-white hover:bg-gray-50"/>
+                                <input type="button" value="Avançar" onClick={()=>setStageForm(stageForm + 1)} className="text-white text-sm/6 px-4 py-2 rounded-full cursor-pointer bg-amber-400 hover:bg-amber-400/80"/>
+                            </div>
+                            
+                            </>
+                        )
+                    }
+                    {
+                        stageForm === 2 && (
+                            <>
+
+                            <FormSelect 
+                            id="uf" 
+                            name="uf" 
+                            className="p-4 font-inter outline-none text-gray-700 rounded-md border border-black/10 focus:border-amber-400"
+                            required
+                            labelText="Em qual estado você reside?"
+                            labelClassName="text-gray-700 font-inter"
+                            options={[
+                                { optionValue: "AC", content: "Acre" },
+                                { optionValue: "AL", content: "Alagoas" },
+                                { optionValue: "AP", content: "Amapá" },
+                                { optionValue: "AM", content: "Amazonas" },
+                                { optionValue: "BA", content: "Bahia" },
+                                { optionValue: "CE", content: "Ceará" },
+                                { optionValue: "DF", content: "Distrito Federal" },
+                                { optionValue: "ES", content: "Espírito Santo" },
+                                { optionValue: "GO", content: "Goiás" },
+                                { optionValue: "MA", content: "Maranhão" },
+                                { optionValue: "MT", content: "Mato Grosso" },
+                                { optionValue: "MS", content: "Mato Grosso do Sul" },
+                                { optionValue: "MG", content: "Minas Gerais" },
+                                { optionValue: "PA", content: "Pará" },
+                                { optionValue: "PB", content: "Paraíba" },
+                                { optionValue: "PR", content: "Paraná" },
+                                { optionValue: "PE", content: "Pernambuco" },
+                                { optionValue: "PI", content: "Piauí" },
+                                { optionValue: "RJ", content: "Rio de Janeiro" },
+                                { optionValue: "RN", content: "Rio Grande do Norte" },
+                                { optionValue: "RS", content: "Rio Grande do Sul" },
+                                { optionValue: "RO", content: "Rondônia" },
+                                { optionValue: "RR", content: "Roraima" },
+                                { optionValue: "SC", content: "Santa Catarina" },
+                                { optionValue: "SP", content: "São Paulo" },
+                                { optionValue: "SE", content: "Sergipe" },
+                                { optionValue: "TO", content: "Tocantins" }
+                            ]}/>
+
+                            <div className="flex flex-1 items-end justify-end gap-6">
+                                <input type="button" value="Retornar" onClick={()=>setStageForm(stageForm - 1)} className="text-gray-800 text-sm/6 px-4 py-2 rounded-full border border-black/10 cursor-pointer bg-white hover:bg-gray-50"/>
+                                <input type="button" value="Avançar" onClick={()=>setStageForm(stageForm + 1)} className="text-white text-sm/6 px-4 py-2 rounded-full cursor-pointer bg-amber-400 hover:bg-amber-400/80"/>
+                            </div>
+
+                            </>
+                        )
+                    }
+
+                    {
+                        stageForm === 3 && (
+                            <>
+
+                            <p className="text-gray-700 font-inter font-medium mb-2">Dados empresariais (opcional)</p>
+                            <FormInput id="bussinesName" label="Nome da fazenda"/>
+
+                            <div className="flex flex-1 items-end justify-end gap-6">
+                                <input type="button" value="Retornar" onClick={()=>setStageForm(stageForm - 1)} className="text-gray-800 text-sm/6 px-4 py-2 rounded-full border border-black/10 cursor-pointer bg-white hover:bg-gray-50"/>
+                                <input type="button" value="Avançar" onClick={()=>setStageForm(stageForm + 1)} className="text-white text-sm/6 px-4 py-2 rounded-full cursor-pointer bg-amber-400 hover:bg-amber-400/80"/>
+                            </div>
+
+                            </>
+                        )
+                    }
+                    {
+                        stageForm === 4 && (
+                            <>
+                            
+                            <p className="text-gray-700 font-inter font-medium mb-2">Segurança da conta</p>
+                            <FormInput id="password" label="Senha"/>
+                            <FormInput id="confirmPassword" label="Confirmar"/>
+                            
+                            <div className="flex gap-6 justify-end">
+                                <input type="button" value="Retornar" onClick={()=>setStageForm(stageForm - 1)} className="text-gray-800 text-sm/6 px-4 py-2 rounded-full border border-black/10 cursor-pointer bg-white hover:bg-gray-50"/>
+                                <button type="submit" className="w-fit py-2 px-4 text-sm/6 text-white font-inter bg-amber-400 rounded-full cursor-pointer select-none transition-colors hover:bg-amber-400/80">Criar conta</button>
+                            </div>
+
+                            </>
+                        )
+                    }
+                </form>
+            </div>
 
             <div className="flex py-4 px-8 items-center">
                 <div className="flex flex-1 gap-4 text-gray-700 text-sm/6 font-inter ">
